d427fc40e87aa63c60c2bb5cafd0d743020ac7d4
Riccardo Licini <rili95@gmail.com>
2020-11-28 23:15:53 +0100

    fixed missing @RequestBody

--- src/main/java/com/riky/spring/starter/rest/StudentsEndpoint.java
+++ src/main/java/com/riky/spring/starter/rest/StudentsEndpoint.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class StudentsEndpoint {
 	private StudentsService service;
 	
 	@PostMapping(path = "/alumni", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void postStudent(Student student) {
+	public void postStudent(@RequestBody Student student) {
 		service.insertStudent(student);
 	}
 	
